1bdf4ff3c894a3b1fcd39fd9504eb8d03c655249
Suraj <surajkumar2091@gmail.com>
2025-01-05 01:02:25 +0530

    40% done

---
+++ src/main/java/com/kumar/springsecuritydemo/Student.java
@@ -0,0 +1,47 @@
+package com.kumar.springsecuritydemo;
+
+
+public class Student {
+    private int id;
+    private String name;
+    private int marks;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", marks=" + marks +
+                '}';
+    }
+
+    public Student(int id, String name, int marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+}
